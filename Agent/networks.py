Fixed the wrong remeber error, and the action normalization

Agent/networks.py
@@ -5,28 +5,37 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense
 
 
-from tensorflow.keras.layers import Input, Dense, LSTM, Concatenate
+from tensorflow.keras.layers import Input, Dense, LSTM, Concatenate, Conv1D
 from tensorflow.keras.models import Model
 
 
-from Config import WINDOW_SIZE, NR_OF_LAYERS    
+from Config import WINDOW_SIZE, NR_OF_LAYERS, FC1_DIMS_LSTM
 
 
-class CriticNetwork(tf.keras.Model):
-    def __init__(self, nr_of_layers=NR_OF_LAYERS, fc1_dims=512, fc2_dims=256,
+class CriticNetwork(Model):
+    def __init__(self, nr_of_layers=4, fc1_dims=512, fc2_dims=256, window_size=100,
                  name='critic', unique_name='hades', chkpt_dir='tmp/ddpg'):
         super(CriticNetwork, self).__init__()
         self.nr_of_layers = nr_of_layers
 
-        self.lstm = LSTM(fc1_dims)  # Initial dimension for LSTM layer
+        # Füge eine Conv1D Schicht hinzu
+        self.conv1d = Conv1D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu', input_shape=(window_size, 1))
+
+        # LSTM Schicht nach der Conv1D
+        self.lstm = LSTM(fc1_dims)
+
+        # Dense Schichten
         self.dense_layers = []
         for i in range(nr_of_layers):
-            if i % 2 == 0:  # Even index layers
+            if i % 2 == 0:
                 self.dense_layers.append(Dense(fc1_dims, activation='relu'))
-            else:           # Odd index layers
+            else:
                 self.dense_layers.append(Dense(fc2_dims, activation='relu'))
-        self.q = Dense(1, activation=None)  # Output layer for Q value
+        
+        # Output Layer für Q-Werte
+        self.q = Dense(1, activation=None)  # Keine Aktivierungsfunktion für Q-Wert Ausgabe
 
+        # Setup für Checkpointing
         self.model_name = f"{name}_{unique_name}"
         self.checkpoint_dir = os.path.join(chkpt_dir, self.model_name)
         os.makedirs(self.checkpoint_dir, exist_ok=True)
@@ -34,35 +43,44 @@ class CriticNetwork(tf.keras.Model):
 
     def call(self, inputs):
         state, action = inputs
-        state = self.lstm(state)
-        combined_input = Concatenate()([state, action])
+        state_processed = self.conv1d(state)
+        state_processed = self.lstm(state_processed)
+        combined_input = Concatenate()([state_processed, action])  # Kombiniere Zustand und Aktion
         for layer in self.dense_layers:
             combined_input = layer(combined_input)
         q = self.q(combined_input)
         return q
 
-class ActorNetwork(tf.keras.Model):
-    def __init__(self, n_actions, nr_of_layers=NR_OF_LAYERS, fc1_dims=512, fc2_dims=256,
-                 name='actor', unique_name='hydra', chkpt_dir='tmp/ddpg'):
+class ActorNetwork(Model):
+    def __init__(self, n_actions, nr_of_layers=4, fc1_dims=512, fc2_dims=256, name='actor', unique_name='hydra', chkpt_dir='tmp/ddpg', window_size=100):
         super(ActorNetwork, self).__init__()
         self.nr_of_layers = nr_of_layers
 
-        self.lstm = LSTM(fc1_dims, input_shape=(WINDOW_SIZE, 1))  # Specifying input shape
+        # Füge eine Conv1D Schicht hinzu
+        self.conv1d = Conv1D(filters=64, kernel_size=3, strides=1, padding='same', activation='relu', input_shape=(window_size, 1))
+
+        # Update LSTM layer to follow Conv1D
+        self.lstm = LSTM(512, return_sequences=False)
+
+        # Dense layers
         self.dense_layers = []
         for i in range(nr_of_layers):
-            if i % 2 == 0:  # Even index layers
+            if i % 2 == 0:
                 self.dense_layers.append(Dense(fc1_dims, activation='relu'))
-            else:           # Odd index layers
+            else:
                 self.dense_layers.append(Dense(fc2_dims, activation='relu'))
+        
         self.mu = Dense(n_actions, activation='tanh')  # Output layer
 
+        # Setup checkpointing
         self.model_name = f"{name}_{unique_name}"
         self.checkpoint_dir = os.path.join(chkpt_dir, self.model_name)
         os.makedirs(self.checkpoint_dir, exist_ok=True)
         self.checkpoint_file = os.path.join(self.checkpoint_dir, f"{self.model_name}_ddpg.h5")
 
-    def call(self, state):
-        state = self.lstm(state)
+    def call(self, inputs):
+        x = self.conv1d(inputs)
+        x = self.lstm(x)
         for layer in self.dense_layers:
-            state = layer(state)
-        return self.mu(state)
+            x = layer(x)
+        return self.mu(x)

Data/BitcoinData.py
@@ -16,10 +16,12 @@ class BitcoinData(IDataSource):
         # Change '24h High (USD)' to your actual closing price column if different
         t = df.index
         #t from 1500 to 2500
-        t = np.arange(1500, 2500)
+        start_index = 1700
+        end_index = 1900
+        t = np.arange(start_index, end_index)
         price = df['24h High (USD)'].values
         #price from 1500 to 2500
-        price = price[1500:2500]
+        price = price[start_index:end_index]
         # df = df[['Date', '24h High (USD)']]  
         # df.columns = ['Days', 'Price']  # Renaming columns to 'Days' and 'Price'
         # self.show_plot(t, price)

Data/MockSyntheticSinusData.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from Data.IDataSource import IDataSource
+from Config import SYNTHETIC_DATA_LENGTH
 
 class MockSyntheticSinusData(IDataSource):
 
@@ -13,7 +14,7 @@ class MockSyntheticSinusData(IDataSource):
     def generate_synthetic_data(self) -> pd.DataFrame:
 
         np.random.seed(123)
-        days = 100
+        days = SYNTHETIC_DATA_LENGTH
         start_price = 240
         end_price = 120
         noise_level = 50  # Significantly increased noise
@@ -33,7 +34,7 @@ class MockSyntheticSinusData(IDataSource):
 
         price = linear_trend + sinusoidal + noise + c
         price = np.maximum(price, 50)
-        self.show_plot(t, price)
+        # self.show_plot(t, price)
         return pd.DataFrame({'Tage': t, 'Preis': price})
 
     def plot_synthetic_data(self, data: pd.DataFrame):